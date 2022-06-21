59a293027be66b34f2df632b6d99002a6e64033c
Andres Hernandez <edanhebla1213@hotmail.com>
2022-06-21 06:42:14 -0500

    worker task

--- worker.js
+++ worker.js
@@ -1,10 +1,10 @@
 const { parentPort } = require('worker_threads');
 
+// receive the parent task
 parentPort.on('message', () => {
   let counter = 0;
   while (counter < 1e9) {
     counter++;
   }
-
   parentPort.postMessage('' + counter);
 });
